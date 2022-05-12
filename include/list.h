e7b213db08ceba588358e88602be4cf4bfdc32b9
avihai_adler <avihai.adler@gmail.com>
2022-05-12 22:32:16 +0300

    added new implementation for linked list

---
+++ include/list.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define INVALID_POS -1
+
+struct node {
+  void *data;
+  struct node *next;
+  struct node *prev;
+};
+
+struct list {
+  int64_t size;
+  struct node *head;
+  struct node *tail;
+};
+
+/* initialize a heap allocated linked list with a size of 0, where head and tail
+ * points to NULL. returns a list on success, NULL on failure
+ */
+struct list *list_init();
+
+/* destroy a list 'object'. if destroy isn't NULL, calls it for every node::data
+ */
+void list_destroy(struct list *list, void (*destroy)(void *data));
+
+/* construct a node with the data passed in. returns a heap allocated node on
+ * success, NULL on failure. internal use only */
+static struct node *init_node(void *data);
+
+/* returns the number of elements on the list. avoid acceessing list::size
+ * directly. use this method instead */
+int64_t list_size(struct list *list);
+
+/* add a node to the start of the list. returns true on success, false otherwise
+ */
+bool list_prepend(struct list *list, void *data);
+
+/* add a node to the end of the list. returns true on success, false otherwise
+ */
+bool list_append(struct list *list, void *data);
+
+/* constructs and inserts a node at position pos. the position is calculated
+ * from the head of the list. returns true on success, false otherwise */
+bool list_insert_at(struct list *list, void *data, int64_t pos);
+
+/* constructs and inserts a node at a position determined by the comparator.
+ * inserts the new node at the first location where new_node::data >
+ * another_node::data */
+bool list_insert_priority(struct list *list, void *data,
+                          int (*cmpr)(const void *, const void *));
+
+/* removes the first node from the list. returns a pointer to the removed
+ * element on success, NULL on failure */
+void *list_remove_first(struct list *list);
+
+/* removes the first node from the list. returns a pointer to the removed
+ * element on success, NULL on failure */
+void *list_remove_last(struct list *list);
+
+/* removes the node at position pos. the position is caluclated from the head of
+ * the list. returns a pointer to the removed element on success, NULL otherwise
+ */
+void *list_remove_at(struct list *list, int64_t pos);
+
+/* finds the first occurence of data and returns its index. the index is
+ * calculated for the list's head. returns a positive number as the index on
+ * success, negative number on failure */
+int64_t list_index_of(struct list *list, void *data,
+                      bool (*equals)(const void *, const void *));
+
+/* replaces an element at position pos. the position is calculated from the
+ * list's head. returns a pointer to the replcaed data on success, NULL
+ * otherwise */
+void *list_replace_at(struct list *list, void *data, int64_t pos);
+
+/* replaces the first occurence of old_data with new_data. returns a pointer to
+ * old_data on success, NULL otherwise */
+void *list_replace(struct list *list, void *old_data, void *new_data,
+                   bool (*equals)(const void *, const void *));
+
+/* sorts the list */
+void list_sort(struct list *list, int (*cmpr)(const void *, const void *));
+
+static void list_split(struct node *src, struct node **front,
+                       struct node **back);
+
+static struct node *list_merge(struct node *front, struct node *back,
+                               int (*cmpr)(const void *, const void *));
+
+static void sort(struct node **src, int (*cmpr)(const void *, const void *));
---
+++ src/list.c
@@ -0,0 +1,283 @@
+#include "include/list.h"
+
+struct list *list_init() {
+  return calloc(1, sizeof(struct list));
+}
+
+void list_destroy(struct list *list, void (*destroy)(void *data)) {
+  if (!list) return;
+
+  for (struct node *tmp = list->head; list->head; tmp = list->head) {
+    list->head = list->head->next;
+
+    if (destroy) {
+      destroy(tmp->data);
+    }
+
+    free(tmp);
+  }
+  free(list);
+}
+
+struct node *init_node(void *data) {
+  struct node *node = calloc(1, sizeof *node);
+  if (!node) return NULL;
+
+  node->data = data;
+  return node;
+}
+
+int64_t list_size(struct list *list) {
+  if (!list) return 0;
+  return list->size;
+}
+
+bool list_prepend(struct list *list, void *data) {
+  if (!list) return false;
+
+  struct node *tmp = init_node(data);
+  if (!tmp) return false;
+
+  if (!list->head) {
+    list->tail = tmp;
+  }
+
+  tmp->next = list->head;
+  list->head = tmp;
+  list->size++;
+  return true;
+}
+
+bool list_append(struct list *list, void *data) {
+  if (!list) return false;
+
+  struct node *tmp = init_node(data);
+  if (!tmp) return false;
+
+  if (!list->tail) {
+    list->head = tmp;
+  }
+
+  tmp->prev = list->tail;
+  list->tail = tmp;
+  list->size++;
+  return true;
+}
+
+bool list_insert_at(struct list *list, void *data, int64_t pos) {
+  if (!list) return false;
+  if (pos < 0) return false;
+  if (pos > list->size) return false;
+
+  if (pos == 0) return list_prepend(list, data);
+  if (pos == list->size) return list_append(list, data);
+
+  struct node *new_node = init_node(data);
+  if (!new_node) return false;
+
+  struct node *tmp = list->head;
+  for (int64_t i = 0; i < pos - 1; i++) {
+    tmp = tmp->next;
+  }
+
+  new_node->prev = tmp;
+  new_node->next = tmp->next;
+  tmp->next->prev = tmp;
+  tmp->next = new_node;
+  list->size++;
+  return true;
+}
+
+bool list_insert_priority(struct list *list, void *data,
+                          int (*cmpr)(const void *, const void *)) {
+  if (!list) return false;
+  if (!list->head) return list_prepend(list, data);
+
+  if (!cmpr) return false;
+
+  // find the node where data > node::data
+  struct node *tmp = list->head;
+  for (; tmp; tmp = tmp->next) {
+    if (cmpr(data, tmp->data) <= 0) continue;
+  }
+
+  // reached the end. all elements are bigger than the new data
+  if (!tmp) return list_append(list, data);
+
+  // the first element is smaller than the new data
+  if (tmp == list->head) return list_prepend(list, data);
+
+  struct node *new_node = init_node(data);
+  if (!new_node) return false;
+
+  new_node->next = tmp;
+  new_node->prev = tmp->prev;
+  tmp->prev->next = new_node;
+  tmp->prev = new_node;
+  list->size++;
+  return true;
+}
+
+void *list_remove_first(struct list *list) {
+  if (!list) return NULL;
+  if (!list->head) return NULL;
+
+  struct node *tmp = list->head;
+
+  // only has 1 element
+  if (list->head == list->tail) {
+    list->tail = list->head = NULL;
+  } else {
+    list->head = list->head->next;
+  }
+  list->size--;
+
+  void *data = tmp->data;
+  free(tmp);
+  return data;
+}
+
+void *list_remove_last(struct list *list) {
+  if (!list) return NULL;
+  if (!list->tail) return NULL;
+
+  struct node *tmp = list->tail;
+
+  // only 1 element
+  if (list->tail == list->head) {
+    list->head = list->tail = NULL;
+  } else {
+    list->tail = list->tail->prev;
+  }
+  list->size--;
+
+  void *data = tmp->data;
+  free(tmp);
+  return data;
+}
+
+void *list_remove_at(struct list *list, int64_t pos) {
+  if (!list) return NULL;
+  if (pos < 0) return NULL;
+  if (pos >= list->size) return NULL;
+
+  if (pos == 0) return list_remove_first(list);
+  if (pos == list->size - 1) return list_remove_last(list);
+
+  struct node *tmp = list->head;
+  for (int64_t i = 0; i < pos; i++) {
+    tmp = tmp->next;
+  }
+
+  tmp->prev->next = tmp->next;
+  tmp->next->prev = tmp->prev;
+  list->size--;
+
+  void *data = tmp->data;
+  free(tmp);
+  return data;
+}
+
+int64_t list_index_of(struct list *list, void *data,
+                      bool (*equals)(const void *, const void *)) {
+  if (!list) return INVALID_POS;
+  if (!equals) return INVALID_POS;
+  if (!list->head) return INVALID_POS;
+
+  int64_t pos = 0;
+  for (struct node *tmp = list->head; tmp; tmp = tmp->next, pos++) {
+    if (equals(tmp->data, data)) return pos;
+  }
+  return INVALID_POS;
+}
+
+void *list_replace_at(struct list *list, void *data, int64_t pos) {
+  if (!list) return NULL;
+  if (!list->head) return NULL;
+  if (pos < 0) return NULL;
+  if (pos >= list->size) return NULL;
+
+  struct node *tmp = list->head;
+  for (int64_t i = 0; i < pos; i++) {
+    tmp = tmp->next;
+  }
+
+  void *old_data = tmp->data;
+  tmp->data = data;
+  return old_data;
+}
+
+/* replaces the first occurence of old_data with new_data. returns a pointer to
+ * old_data on success, NULL otherwise */
+void *list_replace(struct list *list, void *old_data, void *new_data,
+                   bool (*equals)(const void *, const void *)) {
+  int64_t pos = list_index_of(list, old_data, equals);
+  if (pos < 0) return NULL;
+
+  return list_replace_at(list, new_data, pos);
+}
+
+/* sorts the list */
+void list_sort(struct list *list, int (*cmpr)(const void *, const void *)) {
+  if (!list) return;
+  if (!cmpr) return;
+  if (!list->head) return;
+
+  sort(&list->head, cmpr);
+}
+
+void sort(struct node **src, int (*cmpr)(const void *, const void *)) {
+  struct node *head = *src;
+  struct node *front;
+  struct node *back;
+  if (!head || !head->next) return;
+
+  list_split(head, &front, &back);
+
+  sort(&front, cmpr);
+  sort(&back, cmpr);
+
+  *src = list_merge(front, back, cmpr);
+}
+
+void list_split(struct node *src, struct node **front, struct node **back) {
+  struct node *slow = src;
+  struct node *fast = slow->next;
+
+  for (; fast; fast = fast->next) {
+    fast = fast->next;
+    if (fast) {
+      slow = slow->next;
+    }
+  }
+
+  // slow reached the middle of the list;
+  if (slow->next) {
+    slow->next->prev = NULL;
+  }
+
+  *front = src;
+  *back = slow->next;
+
+  slow->next = NULL;
+}
+
+static struct node *list_merge(struct node *front, struct node *back,
+                               int (*cmpr)(const void *, const void *)) {
+  struct node *merged = NULL;
+
+  // base
+  if (!front) return back;
+  if (!back) return front;
+
+  if (cmpr(front->data, back->data) <= 0) {
+    merged = front;
+    merged->next = list_merge(front->next, back, cmpr);
+  } else {
+    merged = back;
+    merged->next = list_merge(front, back->next, cmpr);
+  }
+
+  merged->prev = merged->next->prev;
+  return merged;
+}
