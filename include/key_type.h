8431697908e0870177292ee9bf2050aaac130bf1
avihai_adler <avihai.adler@gmail.com>
2022-05-12 22:49:35 +0300

    removed old impl

--- include/key_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEY_TYPE_H_
-#define KEY_TYPE_H_
-
-#include <stddef.h>
-#include <stdint.h>
-
-enum type { PTR, RAW };
-
-#endif
--- linked_list_tests/include/word_ll_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef WORD_LL_TEST_H_
-#define WORD_LL_TEST_H_
-
-void word_ll_prepend_test(void);
-
-#endif
