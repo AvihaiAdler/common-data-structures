4cd9b42d5a1feb7afb67043af6d01d76c6fda083
adler <avihai.adler@gmail.com>
2023-08-09 15:04:41 +0300

    minor change: - better error codes

--- include/defines.h
+++ include/defines.h
@@ -3,9 +3,11 @@
 #define GROWTH_FACTOR 1
 #define VECT_INIT_CAPACITY 16
 
-#define DS_EINVAL ((size_t)(~0))
-
 enum ds_error {
   DS_OK,
+  DS_VALUE_OK,
   DS_ERROR = -1,
+  DS_NO_MEM = -2,
+  DS_NOT_FOUND = -3,
+  DS_OUT_OF_BOUNDS = -4,
 };
