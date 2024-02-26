aa8eeeb62943404a725aba240432eda3cea8a460
adler <avihai.adler@gmail.com>
2024-02-26 19:57:22 +0200

    major changes: - `queue::_empty` is now set when the queue is full/empty. - added `queue_resize_internal`. - added `queue_enqueue`. - added `queue_dequeue`.

--- src/queue.c
+++ src/queue.c
@@ -1,5 +1,7 @@
 #include "queue.h"
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define QUEUE_INIT_CAPACITY 64
 
@@ -9,10 +11,14 @@ struct queue queue_create(size_t data_size, void (*destroy)(void *_element)) {
   void *data = malloc(data_size * QUEUE_INIT_CAPACITY);
   if (!data_size) { goto empty_queue_lbl; }
 
-  return (struct queue){._capacity = QUEUE_INIT_CAPACITY, ._destroy = destroy, ._data_size = data_size, ._data = data};
+  return (struct queue){._empty = true,
+                        ._capacity = QUEUE_INIT_CAPACITY,
+                        ._destroy = destroy,
+                        ._data_size = data_size,
+                        ._data = data};
 
 empty_queue_lbl:
-  return (struct queue){0};
+  return (struct queue){._empty = true};
 }
 
 void queue_destroy(struct queue *queue) {
@@ -44,9 +50,70 @@ size_t queue_size(struct queue const *queue) {
 bool queue_empty(struct queue const *queue) {
   if (!queue || !queue->_data) return true;
 
-  return queue->_end == queue->_start;
+  return queue->_empty;
 }
 
-bool queue_enqueue(struct queue *restrict queue, void const *restrict element);
+static enum ds_error queue_resize_internal(struct queue *queue) {
+  if (!queue) { return DS_ERROR; }
 
-enum ds_error queue_dequeue(struct queue *restrict queue, void *restrict element);
+  if (queue->_capacity > (SIZE_MAX >> GROWTH_FACTOR) / queue->_data_size) { return DS_NO_MEM; }
+
+  size_t old_capacity = queue->_capacity;
+  queue->_capacity = queue->_capacity << GROWTH_FACTOR;
+
+  void *tmp = realloc(queue->_data, queue->_data_size * queue->_capacity);
+  if (!tmp) { return DS_NO_MEM; }
+
+  if (queue->_end > queue->_start) { goto resize_end; }
+
+  // no micro-optimizations. if queue:_end < queue::_start - queue::_start along with all element up to old_capacity
+  // will be moved. potentially costly of situaltions where queue::_start & queue::_end are at a lower index
+  size_t spacing = old_capacity - queue->_start;
+
+  (void)memmove((char *)tmp + (queue->_capacity - spacing) * queue->_data_size,
+                (char *)tmp + queue->_start * queue->_data_size,
+                spacing * queue->_data_size);
+
+  queue->_start = queue->_capacity - spacing;
+
+resize_end:
+  queue->_data = tmp;
+  return DS_OK;
+}
+
+enum ds_error queue_enqueue(struct queue *restrict queue, void const *restrict element) {
+  if (!queue || !queue->_data) { return DS_ERROR; }
+
+  if (!element) { return DS_ERROR; }
+
+  if (!queue->_empty && queue->_start == queue->_end) {
+    enum ds_error ret = queue_resize_internal(queue);
+
+    if (ret != DS_OK) { return ret; }
+  }
+
+  if (!memcpy((char *)queue->_data + queue->_end * queue->_data_size, element, queue->_data_size)) { return DS_ERROR; }
+
+  queue->_end = (queue->_end + 1) % queue->_capacity;
+  queue->_empty = false;
+
+  return DS_OK;
+}
+
+enum ds_error queue_dequeue(struct queue *restrict queue, void *restrict element) {
+  if (!queue || !queue->_data) { return DS_ERROR; }
+
+  if (!element) { return DS_ERROR; }
+
+  if (queue_empty(queue)) { return DS_NOT_FOUND; }
+
+  if (!memcpy(element, (char *)queue->_data + queue->_start * queue->_data_size, queue->_data_size)) {
+    return DS_ERROR;
+  }
+
+  queue->_start = (queue->_start + 1) % queue->_capacity;
+
+  if (queue->_start == queue->_end) { queue->_empty = true; }
+
+  return DS_VALUE_OK;
+}
