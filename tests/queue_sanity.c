df8aec060be043b81e5836fab941568deec5345c
adler <avihai.adler@gmail.com>
2024-02-28 00:08:08 +0200

    addition: - added some tests for queue-ing after de-queue-ing and vice versa.

--- tests/queue_sanity.c
+++ tests/queue_sanity.c
@@ -96,6 +96,46 @@ static void queue_dequeue_empty_test(void) {
   queue_destroy(&queue);
 }
 
+static void enqueue_some(struct queue *restrict queue, int *restrict arr, size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    enum ds_error ret = queue_enqueue(queue, &arr[i]);
+    assert(ret == DS_OK);
+  }
+}
+
+static void dequeue_some(struct queue *restrict queue, size_t size) {
+  int element;
+  for (size_t i = 0; i < size; i++) {
+    enum ds_error ret = queue_dequeue(queue, &element);
+
+    assert(ret == DS_VALUE_OK);
+  }
+}
+
+static void queue_enqueue_dequeue(int *arr, size_t size) {
+  struct queue queue = queue_create(sizeof *arr, NULL);
+
+  size_t portion_size = size / 16;
+  for (size_t i = 0; i < size; i += portion_size) {
+    enqueue_some(&queue, arr + i, portion_size);
+    dequeue_some(&queue, portion_size);
+  }
+
+  queue_destroy(&queue);
+}
+
+static void queue_enqueue_dequeue_portion(int *arr, size_t size) {
+  struct queue queue = queue_create(sizeof *arr, NULL);
+
+  size_t portion_size = size / 16;
+  for (size_t i = 0; i < size; i += portion_size) {
+    enqueue_some(&queue, arr, portion_size);
+    dequeue_some(&queue, portion_size - 1);
+  }
+
+  queue_destroy(&queue);
+}
+
 static void arr_fill(int *arr, size_t size) {
   for (size_t i = 0; i < size; i++) {
     arr[i] = i + 1;
@@ -118,4 +158,7 @@ int main(void) {
   queue_dequeue_small_test(arr, size);
   queue_dequeue_all_test(arr, size);
   queue_dequeue_empty_test();
+
+  queue_enqueue_dequeue(arr, size);
+  queue_enqueue_dequeue_portion(arr, size);
 }
